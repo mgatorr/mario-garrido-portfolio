69212a4fcbcae8d374d638b8d6f4e2c24c1285cc
Mario Garrido Torres <mgatorr@gmail.com>
2024-09-15 12:41:42 +0200

    feat: Update dependencies and remove unused components

--- components/Footer.js
+++ components/Footer.js
@@ -1,50 +1,51 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
     return (
         <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
             <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
                 <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
-                <div className="flex flex-col justify-between lg:flex-row items-center">
+                <div className="flex flex-col justify-between items-center lg:flex-row">
                     <p>By Mario Garrido</p>
-                    <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
-                        <a
-                            target="_blank"
-                            className={"transition-colors hover:text-yellow-500"}
+                    <div className="flex flex-wrap pt-2 space-x-2 font-medium sm:space-x-4 lg:pt-0">
+                        <Link
                             href="https://twitter.com/mgatorr"
+                            target="_blank"
                             rel="noreferrer"
+                            className="transition-colors hover:text-yellow-500"
                         >
                             Twitter
-                        </a>
-                        <a
-                            target="_blank" 
-                            className={"transition-colors hover:text-yellow-500"}
+                        </Link>
+                        <Link
                             href="https://www.linkedin.com/in/mgarridotorres/"
+                            target="_blank"
                             rel="noreferrer"
+                            className="transition-colors hover:text-yellow-500"
                         >
                             LinkedIn
-                        </a>
-                        <a
-                            target="_blank" 
-                            className={"transition-colors hover:text-yellow-500"}
+                        </Link>
+                        <Link
                             href="https://github.com/MarioGT90k"
+                            target="_blank"
                             rel="noreferrer"
+                            className="transition-colors hover:text-yellow-500"
                         >
                             GitHub
-                        </a>
-                        <a
-                            target="_blank"
-                            className={"transition-colors hover:text-yellow-500"}
+                        </Link>
+                        <Link
                             href="https://dev.to/mariogt"
+                            target="_blank"
                             rel="noreferrer"
+                            className="transition-colors hover:text-yellow-500"
                         >
                             DEV
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Footer;
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,22 +4,19 @@ import ThemeSwitch from "./ThemeSwitch";
 
 const Navigation = () => {
   return (
-    <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
-      <div className="container px-1 mx-auto lg:max-w-1xl flex items-center justify-between">
-        <Link href="/">
-          <a
-            className={
-              "font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
-            }
-          >
-            <p className="text-base"> Mario Garrido</p>
-          </a>
+    <div className="sticky top-0 z-20 py-2 bg-white dark:bg-black md:py-6 md:mb-6">
+      <div className="container mx-auto flex items-center justify-between px-1 lg:max-w-1xl">
+        <Link
+          href="/"
+          className="font-medium text-gray-900 transition-colors hover:text-sky-500 dark:text-white uppercase tracking-wider"
+        >
+          <span className="text-base">Mario Garrido</span>
         </Link>
         <ThemeSwitch />
       </div>
-      <div className="container px-1 mx-auto lg:max-w-1xl flex items-center justify-between">
-        <p className="text-neutral-400 text-base-sm">
-          VP Lead software engineer at JPMorgan Chase & Co.
+      <div className="container mx-auto flex items-center justify-between px-1 lg:max-w-1xl">
+        <p className="text-base-sm text-neutral-400">
+          VP Lead Software Engineer at JPMorgan Chase & Co.
         </p>
       </div>
     </div>
